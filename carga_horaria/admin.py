00432eae0c0af111900f6950ab2ef17e40a47225
Eduardo <don@hso.rocks>
2020-04-02 20:27:05 -0300

    add models to the admin

--- carga_horaria/admin.py
+++ carga_horaria/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Curso
+from .models import Asignatura
+from .models import PlanEstudio
+from .models import Semestre
+from .models import AsignaturaExtra
+from .models import CargaHoraria
+from .models import TablaTiempoLectivo
+from .models import DistribucionTiempoLectivo
 
-# Register your models here.
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Asignatura)
+class AsignaturaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PlanEstudio)
+class PlanEstudioAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Semestre)
+class SemestreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AsignaturaExtra)
+class AsignaturaExtraAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(CargaHoraria)
+class CargaHorariaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TablaTiempoLectivo)
+class TablaTiempoLectivoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(DistribucionTiempoLectivo)
+class DistribucionTiempoLectivoAdmin(admin.ModelAdmin):
+    pass
--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Union
+from .models import Fundacion
+from .models import Colegio
+from .models import ExcelenciaAcademica
+from .models import Periodo
 
-# Register your models here.
+@admin.register(Union)
+class UnionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Fundacion)
+class FundacionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Colegio)
+class ColegioAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ExcelenciaAcademica)
+class ExcelenciaAcademicaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Periodo)
+class PeriodoAdmin(admin.ModelAdmin):
+    pass
