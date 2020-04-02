b2d55a622f388c810682e33d6c5257812b6d9297
Eduardo <don@hso.rocks>
2020-04-02 18:45:04 -0300

    add static urls conf for development

--- carga/urls.py
+++ carga/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+
+admin.site.site_header = 'Carga Académica - Back Office'
 
 urlpatterns = [
     url(r'^scheduler/', include('scheduler.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+# add static
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
