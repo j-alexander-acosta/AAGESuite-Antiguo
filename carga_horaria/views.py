7145080a1072c8761f4a16ab7d165b473b3bdcf0
daniel <danieldelafuente@unach.cl>
2020-04-12 21:52:23 -0400

    Todo listo mi trabajo, llegar y unir al master

--- carga_horaria/views.py
+++ carga_horaria/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from carga_horaria.models import Periodo, Colegio, Plan
-from carga_horaria.forms import PeriodoForm, ColegioForm, PlanForm
-from django.core.urlresolvers import reverse_lazy
+from carga_horaria.formsDani import PeriodoForm, ColegioForm, PlanForm
+from django.core.urlresolvers import reverse_lazy, reverse
 
 
 def home(request):
@@ -19,7 +19,7 @@ class PeriodoListView(ListView):
     model = Periodo
     template_name = 'carga_horaria/periodo/listado_periodos.html'
     search_fields = ['nombre', 'colegio']
-    paginate_by = 3
+    paginate_by = 6
 
 
 class PeriodoDetailView(DetailView):
@@ -43,8 +43,14 @@ class PeriodoUpdateView(UpdateView):
     model = Periodo
     form_class = PeriodoForm
     template_name = 'carga_horaria/periodo/editar_periodo.html'
-#    success_message = u"Periodo %(anio)s actualizado satisfactoriamente."
-#    error_message = "Revise que todos los campos del formulario hayan sido validados correctamente."
+
+    def get_success_url(self):
+        return reverse(
+            'carga-horaria:periodo',
+            kwargs={
+                'pk': self.object.pk,
+            }
+        )
 
 
 class PeriodoDeleteView(DeleteView):
@@ -52,6 +58,8 @@ class PeriodoDeleteView(DeleteView):
     success_url = reverse_lazy('carga-horaria:periodos')
     template_name = 'carga_horaria/periodo/eliminar_periodo.html'
 
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 """
     Fin Crud Periodos
 """
@@ -66,7 +74,7 @@ class ColegioListView(ListView):
     model = Colegio
     template_name = 'carga_horaria/colegio/listado_colegios.html'
     search_fields = ['nombre', 'jec']
-    paginate_by = 3
+    paginate_by = 6
 
 
 class ColegioDetailView(DetailView):
@@ -90,14 +98,24 @@ class ColegioUpdateView(UpdateView):
     model = Colegio
     form_class = ColegioForm
     template_name = 'carga_horaria/colegio/editar_colegio.html'
-#    success_message = u"Periodo %(anio)s actualizado satisfactoriamente."
-#    error_message = "Revise que todos los campos del formulario hayan sido validados correctamente."
+
+    def get_success_url(self):
+        return reverse(
+            'carga-horaria:colegio',
+            kwargs={
+                'pk': self.object.pk,
+            }
+        )
+
 
 
 class ColegioDeleteView(DeleteView):
     model = Colegio
     success_url = reverse_lazy('carga-horaria:colegios')
-    template_name = 'carga_horaria/colegio/eliminar_colegio.html'
+    
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
 
 """
     Fin Crud Colegios
@@ -113,7 +131,7 @@ class PlanListView(ListView):
     model = Plan
     template_name = 'carga_horaria/plan/listado_planes.html'
     search_fields = ['nombre', 'nivel']
-    paginate_by = 3
+    paginate_by = 6
 
 
 class PlanDetailView(DetailView):
@@ -137,15 +155,23 @@ class PlanUpdateView(UpdateView):
     model = Plan
     form_class = PlanForm
     template_name = 'carga_horaria/plan/editar_plan.html'
-#    success_message = u"Periodo %(anio)s actualizado satisfactoriamente."
-#    error_message = "Revise que todos los campos del formulario hayan sido validados correctamente."
+
+    def get_success_url(self):
+        return reverse(
+            'carga-horaria:plan',
+            kwargs={
+                'pk': self.object.pk,
+            }
+        )
 
 
 class PlanDeleteView(DeleteView):
     model = Plan
     success_url = reverse_lazy('carga-horaria:planes')
     template_name = 'carga_horaria/plan/eliminar_plan.html'
-
+    
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 """
     Fin Crud Planes
 """
