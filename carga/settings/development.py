c8ce5e7bc664a9fb8f2c1cb59119addc08268942
Alexis Navarrete <alexisnavarrete@unach.cl>
2021-07-15 16:32:02 -0400

    arreglo de version de psycopg2

--- carga/settings/development.py
+++ carga/settings/development.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
