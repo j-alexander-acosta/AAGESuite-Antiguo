8ad238a558788001fa91dc9214fc3b16b8045722
jorgehalty <jorgehalty@unach.cl>
2021-09-27 11:58:37 -0300

    actuaizado para merge

--- carga/settings/base.py
+++ carga/settings/base.py
@@ -168,7 +168,7 @@ DOMINIO_DEL_SITIO = os.environ.get("DOMINIO_DEL_SITIO")
 
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
